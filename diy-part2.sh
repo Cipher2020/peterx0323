53f685d6e4d17a8eb6d3408b68fde086e3049aff
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-03-23 21:27:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.5.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/Hiwifi/g' package/base-files/files/bin/config_generate
