5f4902d28cbbf5382784f60f4a1f87845ca96dbf
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-07-01 19:56:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,10 +17,7 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 #git clone https://github.com/kenzok8/small-package package/smallpK
 
-git clone https://github.com/kenzok8/small-package package/smallpackage
-git clone https://github.com/kenzok8/small package/small
-git clone https://github.com/kenzok8/openwrt-packages package/opt
