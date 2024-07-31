86a292a67fec301ec6b01da71eb2cd1638ecb61d
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-07-31 13:15:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,7 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 
 echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 
-git clone https://github.com/ximiTech/luci-app-msd_lite.git
+#git clone https://github.com/ximiTech/luci-app-msd_lite.git
 
 #git clone --recursive https://github.com/rozhuk-im/msd_lite.git
 #git clone --recursive https://github.com/syb999/openwrt-19.07.1/tree/master/package/network/services
