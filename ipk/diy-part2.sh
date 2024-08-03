8e0848436797d2148aabf3e5ca653c4783165472
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-08-03 09:44:00 +0800

    Update diy-part2.sh

--- ipk/diy-part2.sh
+++ ipk/diy-part2.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/luci-theme-bootstrap/luci-theme-alpha/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/OpenWrt/jdc/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/CR6609/g' package/base-files/files/bin/config_generate
 
 # Modify Wifi
 sed -i 's/OpenWrt/HiwifiX/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
