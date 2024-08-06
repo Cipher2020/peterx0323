e511a7399c4a17f166ca4151d207180e9df532fb
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-08-06 13:07:12 +0800

    Update diy-part2.sh

--- ipk/diy-part2.sh
+++ ipk/diy-part2.sh
@@ -11,13 +11,13 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.5.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-alpha/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/ImmortalWrt/CR6609/g' package/base-files/files/bin/config_generate
+sed -i 's/opewrt/CR6609/g' package/base-files/files/bin/config_generate
 
 # Modify Wifi
 #sed -i 's/ImmortalWrt/HiwifiX/g' ./package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
