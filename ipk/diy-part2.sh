6ec330b6e860c11f9cc2718fb8ee2039d49988ff
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-08-06 10:31:52 +0800

    Update diy-part2.sh

--- ipk/diy-part2.sh
+++ ipk/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-alpha/g' feeds/luci/collections/luci/Makefile
