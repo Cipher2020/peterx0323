4e32aeb2ad8ebac1fc17dbcd904f28ada6a9d6e8
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-08-06 20:29:22 +0800

    Update diy-part2.sh

--- ipk/diy-part2.sh
+++ ipk/diy-part2.sh
@@ -11,17 +11,17 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.5.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.5.1/192.168.6.1/g' ./package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-alpha/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/openwrt/CR6609/g' package/base-files/files/bin/config_generate
+sed -i 's/openwrt/CR6609/g' ./package/base-files/files/bin/config_generate
 
 # Modify Wifi
 #sed -i 's/ImmortalWrt/HiwifiX/g' ./package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 #sed -i 's/none/psk2/g' ./package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 #sed -i '88i set wireless.default_radio${devidx}.key=987654321' ./package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 #sed -i "186i set wireless.default_radio${devidx}.key=987654321" ./package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
-#rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
