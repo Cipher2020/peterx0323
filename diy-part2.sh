2b508f7192a231fed070afddead717e53b24225d
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-08-07 10:50:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,6 +27,6 @@ sed -i '186i set wireless.default_radio${devidx}.key=987654321' ./package/kernel
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/msd_lite
-git clone https://github.com/syb999/openwrt-19.07.1/tree/master/package/network/services/msd_lite/luci-app-msd_lite feeds\luci\applications
+git clone https://github.com/syb999/openwrt-19.07.1/tree/master/package/network/services/msd_lite/luci-app-msd_lite feeds/luci/applications
 git clone https://github.com/syb999/openwrt-19.07.1/tree/master/package/network/services/msd_lite feeds/packages/net
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
