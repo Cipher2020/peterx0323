ae33f7f869521c3af9835db16087f25b382dd20a
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-08-07 12:01:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,7 +24,7 @@ sed -i 's/OpenWrt/HiwifiX/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.s
 sed -i 's/none/psk2/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '186i set wireless.default_radio${devidx}.key=987654321' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #sed -i "186i set wireless.default_radio${devidx}.key=987654321" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,msd_lite,luci-app-msd_lite}
 rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/msd_lite
 git clone https://github.com/syb999/openwrt-19.07.1/tree/master/package/network/services/msd_lite/luci-app-msd_lite feeds/luci/applications
