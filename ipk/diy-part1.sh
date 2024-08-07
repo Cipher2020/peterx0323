8b51b5087d7bf22666b8eb26669aee19a8b24c1d
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-08-07 12:16:42 +0800

    Update diy-part1.sh

--- ipk/diy-part1.sh
+++ ipk/diy-part1.sh
@@ -18,7 +18,7 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 #echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 
 
