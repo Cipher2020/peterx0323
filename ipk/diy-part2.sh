58205980c6786fdb97471a4ad9ebe7d1c1673873
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-08-09 11:01:42 +0800

    Update diy-part2.sh

--- ipk/diy-part2.sh
+++ ipk/diy-part2.sh
@@ -26,11 +26,11 @@ sed -i 's/openwrt/CR6609/g' ./package/base-files/files/bin/config_generate
 #sed -i "186i set wireless.default_radio${devidx}.key=987654321" ./package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 #rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
-sed -i 's/OpenWrt/HiwifiX/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/OpenWrt/HiwifiX/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/none/psk2/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '186i set wireless.default_radio${devidx}.key=987654321' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #sed -i "186i set wireless.default_radio${devidx}.key=987654321" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,msd_lite,luci-app-msd_lite}
 
-rm -rf feeds/packages/net/msd_lite
+#rm -rf feeds/packages/net/msd_lite
 
