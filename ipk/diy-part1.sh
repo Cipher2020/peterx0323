40369ec9715646b0990e2f26e7b3a2620dd4e01a
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-08-10 18:03:27 +0800

    Update diy-part1.sh

--- ipk/diy-part1.sh
+++ ipk/diy-part1.sh
@@ -19,7 +19,7 @@
 
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
-echo 'src-git cimsd https://github.com/Cipher2020/msd' >>feeds.conf.default
+#echo 'src-git cimsd https://github.com/Cipher2020/msd' >>feeds.conf.default
 
 
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
