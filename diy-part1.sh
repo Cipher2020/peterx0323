e779ac2d499400ea0a5b3bfe3107bc1ad60ba72f
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-08-12 16:26:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,7 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git xupnpd https://github.com/Cipher2020/xupnpd' >>feeds.conf.default
 
 echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 
