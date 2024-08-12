e7539017072e6eb75dc1acf499482707199eaf87
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2024-08-12 16:30:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,9 +15,9 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git xupnpd https://github.com/Cipher2020/xupnpd' >>feeds.conf.default
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 
-echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 #git clone https://github.com/ximiTech/luci-app-msd_lite.git
 
