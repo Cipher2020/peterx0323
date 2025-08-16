1c392c6ef91e2638367b613ac91269d05c0f89bf
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2025-08-17 06:42:16 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,13 +11,13 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-alpha/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/OpenWrt/jdc/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/cr6609/g' package/base-files/files/bin/config_generate
 
 # Modify Wifi
 sed -i 's/OpenWrt/HiwifiX-5/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
