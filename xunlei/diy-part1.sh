b12251f2fe74023498071bbc6f49f89c01e7b484
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2025-08-20 17:00:41 +0800

    Update diy-part1.sh

--- xunlei/diy-part1.sh
+++ xunlei/diy-part1.sh
@@ -20,3 +20,4 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 
 echo 'src-git smpackage https://github.com/Cipher2020/small-package' >>feeds.conf.default
 echo 'src-git kwrt https://github.com/cipher2020/kwrt-packages' >>feeds.conf.default
+echo 'src-git gecoosac https://github.com/Cipher2020/openwrt-gecoosac' >>feeds.conf.default
