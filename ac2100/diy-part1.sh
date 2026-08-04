80f4087a1e54492007505c7b8fbf548ba5e0723e
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2026-08-04 08:41:35 +0800

    Add initial OpenWrt DIY script part 1

---
+++ ac2100/diy-part1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+
+#echo 'src-git smpackage https://github.com/kuoruan/luci-app-v2ray' >>feeds.conf.default
+sed -i '1i src-git smpackage https://github.com/Cipher2020/small-package' feeds.conf.default
+#echo 'src-git smpackage https://github.com/Cipher2020/small-package' >>feeds.conf.default
+#echo 'src-git kwrt https://github.com/cipher2020/kwrt-packages' >>feeds.conf.default
+#echo 'src-git gecoosac https://github.com/Cipher2020/openwrt-gecoosac' >>feeds.conf.default
