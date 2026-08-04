cdc45875f7493369badeba63a906fbaa7d97c900
Cipher2020 <111578191+Cipher2020@users.noreply.github.com>
2026-08-04 08:45:33 +0800

    Create diy-part2.sh

---
+++ ac2100/diy-part2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+sed -i 's/192.168.5.1/192.168.9.1/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/OpenWrt/Reet/g' package/base-files/files/bin/config_generate
+
+# Modify Wifi
+sed -i 's/OpenWrt/LTHB/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/key=987654321/key=lthb6878667/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/none/psk2/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i '186i set wireless.default_radio${devidx}.key=987654321' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i "186i set wireless.default_radio${devidx}.key=987654321" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,filebrowser}
+#删除SMPACKAGE的luci-app-qbittorrent
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
